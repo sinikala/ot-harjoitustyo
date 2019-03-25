843b3f2f1987e3d136c0975ea7a7276d96b1758a
Laura S <46318664+sinikala@users.noreply.github.com>
2019-03-25 23:25:38 +0200

    .gitignore lisätty ja Maksukortin testit lisätty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoOikeinAlussa() {
+        assertTrue(kortti.saldo()==10);      
+    }
+    
+    @Test
+    public void kortinLataaminenToimii() {
+        kortti.lataaRahaa(200);
+        assertTrue(kortti.saldo()==210);      
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinRahaaOtettaessa() {
+        kortti.otaRahaa(5);
+        assertTrue(kortti.saldo()==5);      
+    }
+    
+     @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(15);
+        assertTrue(kortti.saldo()==10);      
+    }
+    
+     @Test
+    public void otaRahaaPalauttaaTrueJosSaldoRiittaa() {
+        assertTrue(kortti.otaRahaa(5));      
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosSaldoEiRiita() {
+        assertFalse(kortti.otaRahaa(15));      
+    }
 }
